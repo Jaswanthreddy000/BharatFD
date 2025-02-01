36688db57f90e604ddd51bf930fd654ae3d1230f
Jaswanthreddy000 <jaswanth6365@gmail.com>
2025-02-01 19:10:15 +0530

    feat: Implement Redis caching for translations

--- faq/views.py
+++ faq/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import FAQ
 from .serializers import FAQSerializer
+from django.core.cache import cache
 
 class FAQViewSet(viewsets.ModelViewSet):
     queryset = FAQ.objects.all()
@@ -8,8 +9,12 @@ class FAQViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         lang = self.request.query_params.get('lang', 'en')
-        queryset = FAQ.objects.all()
-        for faq in queryset:
-            faq.question = faq.get_translated_question(lang)
-            faq.answer = faq.get_translated_answer(lang)
+        cache_key = f'faqs_{lang}'
+        queryset = cache.get(cache_key)
+        if not queryset:
+            queryset = FAQ.objects.all()
+            for faq in queryset:
+                faq.question = faq.get_translated_question(lang)
+                faq.answer = faq.get_translated_answer(lang)
+            cache.set(cache_key, queryset, timeout=60 * 15)  # Cache for 15 minutes
         return queryset
