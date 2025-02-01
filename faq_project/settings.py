c2f42f9d9c79c95b68b8bb0d68d2f490efb1f3b4
Jaswanthreddy000 <jaswanth6365@gmail.com>
2025-02-01 19:06:42 +0530

    feat: Add REST API for FAQs with language support

--- faq_project/settings.py
+++ faq_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'faq',
     'ckeditor',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
