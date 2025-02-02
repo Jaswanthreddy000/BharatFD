3cb3fd5701a579a26578e0a6fff973632e418688
Theeyagura Jaswanth Reddy <116836018+Jaswanthreddy000@users.noreply.github.com>
2025-02-02 19:42:08 +0530

    Update settings.py

--- faq_project/settings.py
+++ faq_project/settings.py
@@ -107,13 +107,15 @@ WSGI_APPLICATION = 'faq_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
